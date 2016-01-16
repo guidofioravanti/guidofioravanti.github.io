9ddd51137f7abaad0bd328d16cf6894084cddf00
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-16 23:12:25 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -3,7 +3,7 @@ window.onload=function(){
 	var map1951 = L.map('map1951').setView([41, 13.5], 6);
 	L.tileLayer.provider('OpenTopoMap').addTo(map1951);
 
-	$.getJSON('https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1951.geojson').addTo(map1951);
+	L.geoJson('https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1951.geojson').addTo(map1951);
 
 
 
