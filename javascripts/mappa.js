ec15eeb1a202ba5983151c05ebe2fde2e3abe18b
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-16 23:20:44 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -3,8 +3,15 @@ window.onload=function(){
 	var map1951 = L.map('map1951').setView([41, 13.5], 6);
 	L.tileLayer.provider('OpenTopoMap').addTo(map1951);
 
-	L.geoJson('https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1951.geojson').addTo(map1951);
-
-
+	$.ajax({
+	    type: "POST",
+	    url: 'https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1951.geojson',
+	    dataType: 'json',
+	    success: function (response) {
+	        geojsonLayer = L.geoJson(response).addTo(map1951);
+	        map.fitBounds(geojsonLayer.getBounds());
+	        $("#info").fadeOut(500);
+	    }
+	});
 
 }
