0332d4fcb29ca515fa488c728dd86bee22610f97
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-16 23:34:22 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -3,13 +3,11 @@ window.onload=function(){
 	var map1951 = L.map('map1951').setView([41, 13.5], 6);
 	//L.tileLayer.provider('OpenTopoMap').addTo(map1951);
 	
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
-			maxZoom: 13,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'mapbox.light'
-		}).addTo(map1951);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', 
+	{
+		maxZoom: 13,
+		id: 'mapbox.light'
+	}).addTo(map1951);
 	
 
 	$.ajax({
