8279ffac2cdbcea1c239b39751a085d218a0bb72
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-17 00:21:25 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -7,11 +7,16 @@ window.onload=function(){
 	
 	//L.tileLayer.provider('OpenTopoMap').addTo(map1951);
 	
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', 
+	var myTile= L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', 
 	{
 		maxZoom: 13,
 		id: 'mapbox.light'
-	}).addTo(map1951);
+	});
+	
+	myTile.addTo(map1951);
+	myTile.addTo(map1961);
+	myTile.addTo(map1971);
+	myTile.addTo(map1981);	
 	
 	var geojsonMarkerOptions = {
 	    radius: 8,
@@ -36,6 +41,51 @@ window.onload=function(){
 	        $("#info").fadeOut(500);
 	    }
 	});
+	
+	$.ajax({
+	    type: "POST",
+	    url: 'https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1961.geojson',
+	    dataType: 'json',
+	    success: function (response) {
+	        geojsonLayer = L.geoJson(response,{
+	    		pointToLayer: function (feature, latlng) {
+	        		return L.circleMarker(latlng, geojsonMarkerOptions);
+	    		}
+		}).addTo(map1961);
+	        map1961.fitBounds(geojsonLayer.getBounds());
+	        $("#info").fadeOut(500);
+	    }
+	});	
+
+	$.ajax({
+	    type: "POST",
+	    url: 'https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1971.geojson',
+	    dataType: 'json',
+	    success: function (response) {
+	        geojsonLayer = L.geoJson(response,{
+	    		pointToLayer: function (feature, latlng) {
+	        		return L.circleMarker(latlng, geojsonMarkerOptions);
+	    		}
+		}).addTo(map1971);
+	        map1971.fitBounds(geojsonLayer.getBounds());
+	        $("#info").fadeOut(500);
+	    }
+	});
+	
+	$.ajax({
+	    type: "POST",
+	    url: 'https://raw.githubusercontent.com/guidofioravanti/guidofioravanti.github.io/master/json/annuali1981.geojson',
+	    dataType: 'json',
+	    success: function (response) {
+	        geojsonLayer = L.geoJson(response,{
+	    		pointToLayer: function (feature, latlng) {
+	        		return L.circleMarker(latlng, geojsonMarkerOptions);
+	    		}
+		}).addTo(map1981);
+	        map1981.fitBounds(geojsonLayer.getBounds());
+	        $("#info").fadeOut(500);
+	    }
+	});	
 
 
 }
