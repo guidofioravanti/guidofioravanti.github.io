5b98261d017069527c7e2685bdc76113beceb734
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-17 01:13:23 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -1,14 +1,17 @@
+var myTile=L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', 
+	{
+		maxZoom: 13,
+		id: 'mapbox.light',
+		attribution: 'Elaborazione dati ISPRA'	
+	});
+
 function mappa51(){
 
 	var map1951 = L.map('map1951').setView([41, 13.5], 6);
 	
 	//L.tileLayer.provider('OpenTopoMap').addTo(map1951);
 	
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', 
-	{
-		maxZoom: 13,
-		id: 'mapbox.light'
-	}).addTo(map1951);
+	myTile.addTo(map1951);
 	
 	var geojsonMarkerOptions = {
 	    radius: 8,
