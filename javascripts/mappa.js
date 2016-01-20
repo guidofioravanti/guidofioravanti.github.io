6106a4cff58d58509f3576c2f403202b6ac685ff
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-20 17:59:45 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -87,7 +87,7 @@ function mappa51(){
 			  var m = L.marker( [punti[i].feature.properties.latitude,punti[i].feature.properties.longitude ],{
 			  	title: punti[i].feature.properties.regione
 			  });
-			  m.bindPop(title);
+			  m.bindPop(punti[i].feature.properties.regione);
 
   			markerClusters.addLayer( m );
 			
