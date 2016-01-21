d28b731242ec5fe52cd34797b31ce2b76f0020b1
guidofioravanti <guidofioravanti@yahoo.it>
2016-01-21 01:13:48 +0100

    Update mappa.js

--- javascripts/mappa.js
+++ javascripts/mappa.js
@@ -22,9 +22,31 @@ var geojsonMarkerOptions = {
 	
 var opzioniPunti= {
 		    	pointToLayer: function(feature, latlng) {
-	    			   	return L.marker(latlng); //, geojsonMarkerOptions
-	    			   	//return L.circleMarker(latlng, geojsonMarkerOptions);
+	    			   	return L.circleMarker(latlng, geojsonMarkerOptions);
 	    		},
+	    		style: function(feature){
+	    			
+			    	if(parseFloat(feature.properties.normale)<= 500){
+		   				return {fillColor: "#ffffd9"};
+		       	    	}else if(feature.properties.normale > 500 && feature.properties.normale<=1000) {
+		    				return {fillColor: "#edf8b1"};
+			    	}else if(feature.properties.normale > 1000 && feature.properties.normale<=1500){
+		    				return {fillColor: "#c7e9b4"};	    		
+			    	}else if(feature.properties.normale > 1500 && feature.properties.normale<=2000){
+		    				return {fillColor: "#7fcdbb"};
+			    	}else if(feature.properties.normale > 2000 && feature.properties.normale<=2500){
+		    				return {fillColor: "#41b6c4"};
+			    	}else if(feature.properties.normale > 2500 && feature.properties.normale<=3000){
+		    				return {fillColor: "#1d91c0"};
+			    	}else if(feature.properties.normale > 3000 && feature.properties.normale<=3500){
+		    				return {fillColor: "#225ea8"};
+			    	}else if(feature.properties.normale > 3500 && feature.properties.normale<=4000){
+		    				return {fillColor: "#253494"};
+			    	}else if(feature.properties.normale > 4000){
+		    				return {fillColor: "#081d58"};
+			    	}	    				
+		    				
+			}, //fine style
 
 	    		onEachFeature: function (feature, layer) {
 				 layer.bindPopup('<b>Nome</b>: '+feature.properties.SiteName.toUpperCase() + '<br><b>Rete</b>: '+feature.properties.regione.toUpperCase()+'<br><b>Normale</b>: '+feature.properties.normale);
@@ -54,9 +76,9 @@ function mappa51(){
 	        var markerClusters = L.markerClusterGroup();	
 
 	    	var geojsonLayer=L.geoJson(response,opzioniPunti);
-//		map1951.fitBounds(geojsonLayer.getBounds());
-		markerClusters.addLayer(geojsonLayer);
-	        map1951.addLayer(markerClusters.getBounds());
+		map1951.fitBounds(geojsonLayer.getBounds());
+//		markerClusters.addLayer(geojsonLayer);
+//	        map1951.addLayer(markerClusters.getBounds());
 	    }
 	});
 	
